55dca75fb204fb3d8aa16cee0f108c162762c768
rohith2842005 <musunururohith@gmail.com>
2025-07-08 15:53:45 +0530

    ns

--- client/src/pages/News/Tech.js
+++ client/src/pages/News/Tech.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./News.css";
 
-const Cyber = () => {
+const Tech= () => {
   const [mynews, setMyNews] = useState([]);
 
   const fetchData = async () => {
